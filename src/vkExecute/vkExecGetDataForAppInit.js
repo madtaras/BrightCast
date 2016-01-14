5261152c848db6c9bbb67bc91a1017e76d1b24b2
Taras Mazurkevych <mad.taras@gmail.com>
2016-01-15 01:37:34 +0200

    Fix app can't download init data because of VK error 13 "Too many operations"

--- src/vkExecute/vkExecGetDataForAppInit.js
+++ src/vkExecute/vkExecGetDataForAppInit.js
@@ -2,33 +2,36 @@
 var response = {};
 
 // Getting user name and small(50x50) avatar
-var userInfo = API.users.get({
+response.userInfo = API.users.get({
     "user_id": Args.user_id,
     "access_token": Args.access_token,
     "name_case": "Nom",
     "fields": "photo_50",
     "v": 5.40})[0];
-response.userInfo = userInfo;
 
 
 // Getting user's audios
-var userAudios = API.audio.get({
+response.userAudios = API.audio.get({
     "user_id": Args.user_id,
     "access_token": Args.access_token,
     "v": 5.40,
     "count": 40});
-response.userAudios = userAudios;
+
 
 // Getting user's audios count
-var userAudiosCount = API.audio.getCount({
+response.userAudiosCount = API.audio.getCount({
     "user_id": Args.user_id,
     "access_token": Args.access_token,
     "owner_id": Args.user_id});
-response.userAudiosCount = userAudiosCount;
+
 
 // Getting friends with opened audio
-var friendsWithOpenedAudios = [];
-var allFriendsArray = API.friends.get( {user_id: Args.user_id} );
+response.friendsWithOpenedAudios = [];
+var allFriendsArray = API.friends.get({
+    "user_id": Args.user_id,
+    "order": "hints"
+});
+
 var allFriendsString = "";
 // converting arrFriendsArray to string
 while (allFriendsArray.length) {
@@ -41,33 +44,30 @@ allFriendsArray = API.users.get({
     "access_token": Args.access_token,
     "v": 5.40});
 
-while (allFriendsArray.length) {
+while (!!allFriendsArray.length && (response.friendsWithOpenedAudios.length < 100 )) {
     if (allFriendsArray[0].can_see_audio) {
-        friendsWithOpenedAudios.push(allFriendsArray[0]);
+        response.friendsWithOpenedAudios.push(allFriendsArray[0]);
     }
     allFriendsArray.shift();
 }
-response.friendsWithOpenedAudios = friendsWithOpenedAudios;
 
 
 // Getting user's groups
-var userGroups = API.groups.get({
+response.userGroups = API.groups.get({
     "user_id": Args.user_id,
     "access_token": Args.access_token,
     "v": 5.40,
     "count": 1000,
     "extended": 1});
-response.userGroups = userGroups;
 
 
 // Getting friends who broadcast
-var broadcastingFriends = API.audio.getBroadcastList({
+response.broadcastingFriends = API.audio.getBroadcastList({
     "user_id": Args.user_id,
     "access_token": Args.access_token,
     "v": 5.40,
     "active": 1
 });
-response.broadcastingFriends = broadcastingFriends;
 
 
 // returning response object
