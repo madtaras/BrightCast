f1ef922eab49d389cd3ad7953ad5b288d341485b
Taras Mazurkevych <mad.taras@gmail.com>
2016-02-23 14:16:06 +0200

    Remove cleaning user's broadcast on app close

--- src/js/background.js
+++ src/js/background.js
@@ -9,10 +9,6 @@ chrome.app.runtime.onLaunched.addListener(function () {
     }
   })
 
-  function objectToRequestParam (obj) {
-    return '?' + Object.keys(obj).reduce(function (a, k) { a.push(k + '=' + encodeURIComponent(obj[k])); return a }, []).join('&')
-  }
-
   function openMainPage () {
     chrome.app.window.create('desktopApp.html', {
       'id': 'main-page',
@@ -28,16 +24,6 @@ chrome.app.runtime.onLaunched.addListener(function () {
       var mainPage = chrome.app.window.get('main-page')
       mainPage.contentWindow.addEventListener('logOut', function () {
         mainPage.close()
-      })
-      mainPage.onClosed.addListener(function () {
-        chrome.storage.local.get(['vkAccessToken', 'vkUserID'], function (userAuthData) {
-          var requestOptions = {
-            'access_token': userAuthData.vkAccessToken,
-            'user_id': userAuthData.vkUserID,
-            'audio': ''
-          }
-          window.fetch('https://api.vk.com/method/audio.setBroadcast' + objectToRequestParam(requestOptions))
-        })
       })
     })
   }
